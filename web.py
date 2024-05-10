3b19798476d8f712b3a8ac3b06c9f3ff833bef11
s28381 <s28381@pjwstk.edu.pl>
2024-05-11 00:01:20 +0200

    Test files

---
+++ web.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="ZipReader", page_icon=":tada:", layout="wide")
+
+st.subheader("ZipReader")
+st.title("ZipReader")
+st.write("This app allows you to")
